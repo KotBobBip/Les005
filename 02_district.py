09185d7b611ed1d5b09b1f81e8e6a466cdd32072
PavLove <gorilla33333@gmail.com>
2023-10-22 13:38:35 +0300

    HW 2

--- 02_district.py
+++ 02_district.py
@@ -5,7 +5,32 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from district.central_street.house1 import room1 as cs_h1_r1
+from district.central_street.house1 import room2 as cs_h1_r2
+from district.central_street.house2 import room1 as cs_h2_r1
+from district.central_street.house2 import room2 as cs_h2_r2
+
+from district.soviet_street.house1 import room1 as ss_h1_r1
+from district.soviet_street.house1 import room2 as ss_h1_r2
+from district.soviet_street.house2 import room1 as ss_h2_r1
+from district.soviet_street.house2 import room2 as ss_h2_r2
+
+
+total_folks = []
+total_folks = total_folks \
+              + cs_h1_r1.folks \
+              + cs_h1_r2.folks \
+              + cs_h2_r1.folks \
+              + cs_h2_r2.folks \
+              + ss_h1_r1.folks \
+              + ss_h1_r2.folks \
+              + ss_h2_r1.folks \
+              + ss_h2_r2.folks
+
+
+total_folks_str = ",".join(total_folks)
+
+print(total_folks_str)
 
 
 
